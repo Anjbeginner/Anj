9e3f04b6d7e7d33cb71784c6586cc0a8f0bd8ac6
Anjbeginner <88421655+Anjbeginner@users.noreply.github.com>
2021-08-04 12:35:37 +0530

    Create Picking Numbers.py

---
+++ Picking Numbers.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    maxi=0
+    diff=1
+    for i in a:
+        n1=a.count(i)
+        n2=a.count(i-diff)
+        maxi=max(maxi,n1+n2)
+    return maxi
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
