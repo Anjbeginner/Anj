53146ebd4f9e1eeea1084ab52ceb1668ea6f591b
Anjbeginner <88421655+Anjbeginner@users.noreply.github.com>
2021-08-14 02:11:02 +0530

    Create viral ad.py

---
+++ viral ad.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'viralAdvertising' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def viralAdvertising(n):
+    ad=5
+    total=0
+    for i in range(n):
+        y=ad//2  #floor divison
+        total+=y
+        ad=y*3
+    return total
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
