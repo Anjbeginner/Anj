b6641c2afbba768fb7c4683c0bf7d67d43443b21
Anjbeginner <88421655+Anjbeginner@users.noreply.github.com>
2021-10-23 09:59:50 +0530

    Create evenoddcheck.cpp

---
+++ evenoddcheck.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    cout<<"enter number";
+    cin>>i;
+    if(i%2==0){
+        cout<<i<<" is even";
+    }
+    else{
+        cout<<i<<"is odd";
+    }
