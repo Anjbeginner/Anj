f537fc5d3efa2648f3c35c236d3cd8855a2393e1
Anjbeginner <88421655+Anjbeginner@users.noreply.github.com>
2021-10-23 09:49:42 +0530

    Create evenodd.cpp

---
+++ evenodd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i=0;i<=10;i++){
+        if(i%2!=0){
+            cout<<i<<endl;
+        }
+        
+    } 
+
+    return 0;
+}
